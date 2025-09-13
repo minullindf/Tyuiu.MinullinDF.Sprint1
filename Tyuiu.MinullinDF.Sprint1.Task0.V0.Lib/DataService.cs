88245d8648d2295e796104128678fa96f904bb8d
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-09-13 12:37:28 +0500

    Task0_1

--- Tyuiu.MinullinDF.Sprint1.Task0.V0.Lib/DataService.cs
+++ Tyuiu.MinullinDF.Sprint1.Task0.V0.Lib/DataService.cs
@@ -3,7 +3,7 @@ using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.MinullinDF.Sprint1.Task0.V0.Lib
 {
-    public class DataService : ISprint1Task0V0
+    public class DataService : ISprint1Task0V28
     {
         public double Calculate()
         {
