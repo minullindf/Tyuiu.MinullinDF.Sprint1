e4dc71c4c2a79ca8eb7c5b0557d1e3709e8ffa73
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-09-13 12:07:56 +0500

    Add project files.

---
+++ Tyuiu.MinullinDF.Sprint1.Task0.V0.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.MinullinDF.Sprint1.Task0.V0.Lib
+{
+    public class DataService : ISprint1Task0V0
+    {
+        public double Calculate()
+        {
+            return 10 / (2 + 3);
+        }
+    }
+}
---
+++ Tyuiu.MinullinDF.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MinullinDF.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.MinullinDF.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
