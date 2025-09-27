b04028611526930a45a56e3bb9248d8eb874ab42
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-09-27 13:34:39 +0500

    Task2

---
+++ Tyuiu.MinullinDF.Sprint1.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MinullinDF.Sprint1.Task2.V7.Lib;
+
+namespace Tyuiu.MinullinDF.Sprint1.Task2.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            var res = ds.CalculateSquareCircle(x);
+            Assert.AreEqual(3.14, res);
+        }
+    }
+}
---
+++ Tyuiu.MinullinDF.Sprint1.Task2.V7/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MinullinDF.Sprint1.Task2.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        string zv = new string('*', 75);
+
+        Console.Title = "Спринт #1 | Выполнил: Минуллин Д. Ф. | АСОиУБ-25-1";
+        Console.WriteLine(zv);
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: выполняет указанные расчёты и печатает результат на экране        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Минуллин Динар Фаатович | АСОиУБ-25-1                         *");
+        Console.WriteLine(zv);
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine(zv);
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine(zv);
+
+        int r;
+        Console.WriteLine("Введите значение радиуса:");
+        r = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(zv);
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine(zv);
+
+        Console.WriteLine(ds.CalculateSquareCircle(r));
+
+        Console.ReadLine();
+    }
+}
