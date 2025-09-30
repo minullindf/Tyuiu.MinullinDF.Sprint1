3e650d56cbb78215cc18eba41afce092b5149d03
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-09-30 23:26:21 +0500

    Task7

---
+++ Tyuiu.MinullinDF.Sprint1.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MinullinDF.Sprint1.Task7.V5.Lib;
+namespace Tyuiu.MinullinDF.Sprint1.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x = 2;
+            DataService ds = new DataService();
+            var res = ds.Calculate(x);
+            Assert.AreEqual(-0.545, res);
+        }
+    }
+}
---
+++ Tyuiu.MinullinDF.Sprint1.Task7.V5/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.MinullinDF.Sprint1.Task7.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("      ln(|cos(x)|)");
+        Console.WriteLine("z = ----------------");
+        Console.WriteLine("                2");
+        Console.WriteLine("        ln(1 + x )");
+
+        double x;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(x));
+        Console.ReadKey();
+    }
+}
